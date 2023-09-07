fda0a0a88e24a5bb72c034b16d645792c0bda326
Surribas1980 <issupl@gmail.com>
2023-09-07 11:33:07 +0000

    Engadindo a lista

--- static/javascript/helpers.js
+++ static/javascript/helpers.js
@@ -21,14 +21,25 @@ const pintarLista = () => {
   if(typeof listaCompra === "string"){
     listaConvertida = JSON.parse(listaCompra)
     console.log(listaConvertida,typeof listaConvertida)
+    let resultado = 0;
     const pintar = listaConvertida.map((item)=>{
           let elementoLista = document.createElement('li');
           
           elementoLista.innerHTML = `${item.producto} ${item.valor} ${item.cantidade}`;
+          if(item.cantidade != undefined && item.cantidade != null){
+            
+            resultado += parseFloat(item.valor)*item.cantidade;
+            console.log('resultado ', resultado)
+          }
           lista.prepend(elementoLista)
           
       //console.log(item.producto,item.cantidade,item.valor)
     })
+
+    let resultadoPintado = document.createElement('div');
+    
+    resultadoPintado.innerHTML= `Total: ${resultado} €`;
+    lista.after(resultadoPintado)
   }
 }
 export {
